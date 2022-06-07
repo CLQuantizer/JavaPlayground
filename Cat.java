228b67d627a996f028c43107b43d84ad30f9ab5d
CLQuantizer <ezioxxk@gmail.com>
2022-06-07 21:40:18 +0100

    cat main is here

--- Cat.java
+++ Cat.java
@@ -1,4 +1,8 @@
 class Cat extends Animal {
+    static public void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+    }
     public void eat() {
         System.out.println("Cat is eating");
     }
