4bbf178a958e7f71bc917a256e577d1b37d24712
CLQuantizer <ezioxxk@gmail.com>
2022-06-07 21:41:04 +0100

    dog main is here

--- Dog.java
+++ Dog.java
@@ -1,4 +1,8 @@
 class Dog extends Animal {
+    static public void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+    }
     public void eat() {
         System.out.println("Dog is eating");
     }
