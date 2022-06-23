db820ba685cdd27aa020e565409601881adb6392
CLQuantizer <ezioxxk@gmail.com>
2022-06-23 16:36:44 +0100

    good

---
+++ FinalDemo.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class FinalDemo {
+    final static private List<Integer> finalList = new ArrayList<Integer>();
+    static private List<Integer> nonFinalList = new ArrayList<Integer>();
+    final static private Integer finalInteger = new Integer(1);
+    public static void main(String[] args) {
+        finalList.add(1);
+        finalList.add(2);
+        nonFinalList.add(3);
+        nonFinalList.add(4);
+        System.out.println(finalList);
+        // finalInteger=new Integer(1);
+        nonFinalList = Arrays.asList(1,2,3,4);
+        System.out.println(nonFinalList);
+    }
+}
