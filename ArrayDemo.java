b8c649a4ca7e8bb904b31d298e5e8236d1488009
CLQuantizer <ezioxxk@gmail.com>
2022-06-24 20:12:26 +0100

    Arrays.asList() cannot give you an object with .add() method

---
+++ ArrayDemo.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+class ArrayDemo {
+    public static void main(String[] args) {
+        Integer[] integerArray = {1, 2, 3, 4, 5};
+        List<Integer> intList = Arrays.asList(integerArray);
+        //This can compile but will not work
+        intList.add(6);
+    }
+
+}
