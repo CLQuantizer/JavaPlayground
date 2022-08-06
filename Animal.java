3f4e070ed37cef8ccf9a62c7c7218243cc0b7f09
Ez <ezioxxk@gmail.com>
2022-08-06 14:28:40 +0100

    I forgot to commit these two to PriorityQueue Exercise

--- Animal.java
+++ Animal.java
@@ -1,3 +1,14 @@
-abstract class Animal {
-    public abstract void eat();
+public abstract class Animal implements Comparable<Animal> {
+	private String name;
+	public Animal(){
+		System.out.println("No arg Animal constructed");
+	}
+	public Animal(String name){
+		this.name=name;
+		System.out.println("Arg Animal constructed");
+	}
+	public abstract void eat();
+	public String getName(){
+		return this.name;
+	}
 }
--- Dog.java
+++ Dog.java
@@ -1,9 +1,22 @@
-class Dog extends Animal {
+class Dog extends Animal{
+    
+    public Dog(){
+	    System.out.println("No arg dog");
+    }
+    public Dog(String name){
+	    super(name);
+	    System.out.println("Super.name is: "+ super.getName());
+	    System.out.println("Arg dog being constructed\n-----------");
+    }
+    public  int compareTo(Animal Another){
+	    return Another.getName().compareTo(super.getName());
+    }
+
     static public void main(String[] args) {
         Dog dog = new Dog();
         dog.eat();
     }
     public void eat() {
-        System.out.println("Dog is eating");
+        System.out.println(super.getName() + " is eating");
     }
 }
