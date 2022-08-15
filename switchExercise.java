e095816beb37cc07ee889782bd0c3bb51122abd2
Ez <ezioxxk@gmail.com>
2022-08-15 08:57:07 +0100

    switch couples with break

---
+++ switchExercise.java
@@ -0,0 +1,45 @@
+class switchExercise{
+    public static void main(String[] args){
+        char letter = 'a';
+        switch(letter){
+            case 'a':
+                System.out.println("a");
+            case 'b':
+                System.out.println("b");
+            case 'c':
+                System.out.println("c");
+            default:
+                System.out.println("default");
+        }
+        // int[] num = {11,2,2,3,4,5,6,7,8,9,10};
+        // for(int i=0;i<num.length;i++){
+        //     System.out.println("Number is: "+num[i]);
+        //     switch(num[i]){
+        //         case 1:
+        //             System.out.println("One");
+        //         case 2:
+        //             System.out.println("Two");
+        //         case 3:
+        //             System.out.println("Three");
+        //         case 4:
+        //             System.out.println("Four");
+        //         case 5:
+        //             System.out.println("Five");
+        //         case 6:
+        //             System.out.println("Six");
+        //         case 7:
+        //             System.out.println("Seven");
+        //         case 8:
+        //             System.out.println("Eight");
+        //         case 9:
+        //             System.out.println("Nine");
+        //         case 10:
+        //             System.out.println("Ten");
+                    
+        //         default:
+        //             System.out.println("Invalid number");
+        // }
+        
+        // }
+    }
+}
